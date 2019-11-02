bf18ef9e008e28365407fca464a3499f5bfa49ed
Sandor Dargo <sandordargo@users.noreply.github.com>
2019-11-02 21:57:30 +0100

    Ex 12: Tower can fire to the closest monster if that is within range

--- monster.py
+++ monster.py
@@ -3,5 +3,11 @@ class Monster:
         self.position = 0
         self.health_point = 10
 
+    def __repr__(self):
+        return "Monster(position={}, health_point={})".format(self.position, self.health_point)
+
     def move_forward(self):
         self.position += 1
+
+    def get_position(self):
+        return self.position
--- tower.py
+++ tower.py
@@ -1,3 +1,5 @@
+import monster
+
 class Tower:
     def __init__(self, position, firePower, lengthOfRegeneration):
         self.position = position
@@ -7,6 +9,14 @@ class Tower:
 
     def decreaseNextFireTime(self):
         self.canFireInNTurns -= 1
+
+    def fireIfMonsterInRange(self, monsters):
+        monsters.sort(key=lambda x: self.position - x.get_position())
+        candidate = monsters[0]
+        if abs(self.position - candidate.get_position()) <= self.firePower and self.canFireInNTurns == 0:
+            candidate.health_point -= 1
+            self.canFireInNTurns = self.lengthOfRegeneration
+        return monsters
 
     def __repr__(self):
         return str(self.firePower)
